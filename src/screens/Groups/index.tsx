98b0278a16744ed4ac0069e2939949862dc633ab
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-11-19 17:41:35 -0300

    feat: add items groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,22 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export const Groups: React.FC = () => {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 };
