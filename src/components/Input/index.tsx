c2066ef5678f91aebce799d52bdade7ca5417aa5
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-12-12 12:56:27 -0300

    feat: new screen player

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,8 +3,6 @@ import { useTheme } from 'styled-components';
 import { TextInputProps, View } from 'react-native';
 import { Container } from './styles';
 
-// import { Container } from './styles';
-
 const Input: React.FC<TextInputProps> = ({ ...rest }) => {
   const { COLORS } = useTheme();
   return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,19 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import React from 'react';
+
+import { Container } from './styles';
+
+const Players: React.FC = () => {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+    </Container>
+  );
+};
+
+export default Players;
