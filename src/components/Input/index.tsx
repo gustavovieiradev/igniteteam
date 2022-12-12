ce00c3018d13e9697305ab7c82914bafd0df3278
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-12-12 12:51:31 -0300

    feat: style placeholder input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 import { TextInputProps, View } from 'react-native';
 import { Container } from './styles';
 
 // import { Container } from './styles';
 
 const Input: React.FC<TextInputProps> = ({ ...rest }) => {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 };
 
 export default Input;
