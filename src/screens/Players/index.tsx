9539452252d400223d771a4fbf3932dc36d298f0
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-12-14 18:11:04 -0300

    feat: list and count filter/player

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,11 +3,14 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import Input from '@components/Input';
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
 
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 const Players: React.FC = () => {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Gustavo', 'Joao', 'Maria']);
   return (
     <Container>
       <Header showBackButton />
@@ -19,7 +22,22 @@ const Players: React.FC = () => {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" isActive={true} />
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 };
