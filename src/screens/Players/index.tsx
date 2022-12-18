a33042858d805fbeed4e7694535fea2dc3492289
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-12-18 14:36:20 -0300

    feat: finishing list player

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button';
 import ButtonIcon from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import Input from '@components/Input';
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayCard } from '@components/PlayCard';
 import React, { useState } from 'react';
 import { FlatList } from 'react-native';
@@ -44,7 +46,16 @@ const Players: React.FC = () => {
         data={players}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <PlayCard name={item} onRemove={() => {}} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+      <Button title="Remove turma" type="SECONDARY" />
     </Container>
   );
 };
