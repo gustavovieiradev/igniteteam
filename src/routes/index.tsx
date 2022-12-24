b88061eca882e1590c47522225cfb852abf57d61
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-12-24 19:28:48 -0300

    fix: glitch navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { View } from 'react-native';
+import { useTheme } from 'styled-components/native';
 import { AppRoutes } from './app.routes';
 
 export const Routes: React.FC = () => {
+  const { COLORS } = useTheme();
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 };
